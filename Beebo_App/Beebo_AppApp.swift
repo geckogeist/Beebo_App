ab8b844a37e86868127491277bd480d284844472
Ethan Nkrumah <ethannkrumah4@gmail.com>
2025-11-21 08:39:43 -0500

    Initial Commit

---
+++ Beebo_App/Beebo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Beebo_AppApp.swift
+//  Beebo_App
+//
+//  Created by Ethan Nkrumah on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Beebo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Beebo_App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Beebo_App
+//
+//  Created by Ethan Nkrumah on 11/21/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
