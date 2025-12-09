eaf578e7e3e47357f8a69368f8d1135b3c892760
Ethan Nkrumah <ethannkrumah4@gmail.com>
2025-12-09 09:35:30 -0500

    home view changes and settings

--- Beebo_App/HomeView.swift
+++ Beebo_App/HomeView.swift
@@ -4,48 +4,64 @@ struct HomeView: View {
     @State private var taskCompleted = 0.0
     @State private var maxTask = 20.0
     @State private var userLevel = 1
-    
+
     private var progressValue: Double {
         taskCompleted / maxTask
     }
-    
+
     var body: some View {
-        VStack(spacing:-30) {
-            
-            NavigationLink(destination: SettingsView()) {
-                Label("Settings", systemImage: "gearshape")
-            }
-            Spacer()
-            ProgressView("Task Progress", value: progressValue, total: 1.0)
+        NavigationStack {
+            VStack(spacing: -30) {
+                
+                
+                ProgressView("Task Progress", value: progressValue, total: 1.0)
+                    .padding()
+                
+                HStack {
+                    Text("\(Int(taskCompleted))/\(Int(maxTask))")
+                    Spacer()
+                }
+                .padding(20)
+
+                HStack {
+                    Text("Level \(userLevel)")
+                        .font(.system(size: 24))
+                    Spacer()
+                }
+                .padding(20)
+                .bold()
+
+                Button("Increment Progress") {
+                    if taskCompleted < maxTask {
+                        taskCompleted += 1.0
+                    } else {
+                        taskCompleted = 0.0
+                    }
+                }
                 .padding()
-            
-            HStack {
-                Text("\(Int(taskCompleted))/\(Int(maxTask))")
-                Spacer()
             }
-            .padding(20)
-            HStack {
-                Text("Level " + String(userLevel))
-                    .font(.system(size: 24))
-
-                Spacer()
-            }
-            .padding(20)
-            .bold()
-
-            Button("Increment Progress") {
-                if taskCompleted < maxTask {
-                    taskCompleted += 1.0
-                } else {
-                    taskCompleted = 0.0  // Reset
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink(destination: SettingsView()) {
+                        Image(systemName: "gear")
+                        
+                    }
                 }
             }
-            .padding()
+            
+            HStack {
+                Text("Latest Task")
+                .cornerRadius(10)
+                .overlay(
+                    RoundedRectangle(cornerRadius: 10)
+                    .stroke(Color(.green), lineWidth: 1))
+                .padding([.top, .horizontal])
+            }
+            Spacer()
         }
     }
 }
 
-#Preview {
+#Preview{
     HomeView()
 }
-
